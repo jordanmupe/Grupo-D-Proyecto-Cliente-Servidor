975f6d2b6b24f03c721cb932c7bd16dee0d08738
Samifide <samiperez0311@gmail.com>
2024-11-20 22:08:13 -0600

    Restaurante
    básicos

--- Restaurante.java
+++ Restaurante.java
@@ -1,2 +1,65 @@
 public class Restaurante {
+    private int Id;
+    private String RestaurantName;
+    private String Address;
+    private String Schedule;
+    private String Rating;
+
+    public Restaurante() {
+    }
+
+    public Restaurante(int id, String restaurantName, String address, String schedule, String rating) {
+        this.Id = id;
+        this.RestaurantName = restaurantName;
+        this.Address = address;
+        this.Schedule = schedule;
+        this.Rating = rating;
+    }
+
+    public int getId() {
+        return this.Id;
+    }
+
+    public void setId(int id) {
+        this.Id = id;
+    }
+
+    public String getRestaurantName() {
+        return this.RestaurantName;
+    }
+
+    public void setRestaurantName(String restaurantName) {
+        this.RestaurantName = restaurantName;
+    }
+
+    public String getAddress() {
+        return this.Address;
+    }
+
+    public void setAddress(String address) {
+        this.Address = address;
+    }
+
+    public String getSchedule() {
+        return this.Schedule;
+    }
+
+    public void setSchedule(String schedule) {
+        this.Schedule = schedule;
+    }
+
+    public String getRating() {
+        return this.Rating;
+    }
+
+    public void setRating(String rating) {
+        this.Rating = rating;
+    }
+
+    public void ReceiveNewOrders(){}
+
+    public void UpdateMenu(){}
+
+    public void UpdateAvailability(){}
+
 }
